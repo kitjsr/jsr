080febafad590268bcaffb488dc66b3a4d829666
Anushruti Mahato <anushruti5882@gmail.com>
2025-06-05 22:02:33 +0530

    home about contact routes nd route

--- src/App.js
+++ src/App.js
@@ -4,17 +4,36 @@ import { Header } from './Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Footer } from './Footer';
 import { Mainarea } from './Mainarea';
+import { Contact } from './Contact';
+import { Home } from './Home';
+import { About } from './About';
+import { Link, Route, Router, Routes } from 'react-router';
 
 
 function App()
 {
   return (
     <div>
-      <h1>JAMSHEDPUR</h1>
-      <p>Jamshedpur is the Steel city of Jharkhand.</p>
+      {/* <h1>JAMSHEDPUR</h1>
+      <p>Jamshedpur is the Steel city of Jharkhand.</p> */}
+      <ul>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+      {/* <Home></Home>
+      <About></About>
+      <Contact></Contact>
       <Header></Header>
       <Mainarea></Mainarea>
-      <Footer></Footer>
+      <Footer></Footer> */}
+      <Routes>
+        <Route path='' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='*' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <div>
+        <h1>CONTACT</h1>
+    </div>
+  )
+}
